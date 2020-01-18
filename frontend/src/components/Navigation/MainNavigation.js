275d252c1041eaff9c0f04aafae986b8d465a321
gabhijeetxsingh <abhijeetvns03@gmail.com>
2020-01-18 21:47:09 +0530

    added code to handle pages according to authentication

--- frontend/src/components/Navigation/MainNavigation.js
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
-
+import AuthContext from '../../context/auth-context';
 import "./mainNavigation.css"
 
 const mainNavigation = props => (
-    <header className="main-navigation">
-        <div className="main-navigation__logo">
-        <h1>Easy Event</h1>
-        </div>
-        <nav className="main-navigation__items">
-            <ul>
-                <li><NavLink to="/auth">Authentication</NavLink></li>
-                <li><NavLink to="/events">Events</NavLink></li>
-                <li><NavLink to="/Bookings">Bookings</NavLink></li>
-            </ul>
-        </nav>
-    </header>
+    <AuthContext.Consumer>
+        {(context)=>{
+            return (<header className="main-navigation">
+            <div className="main-navigation__logo">
+            <h1>Easy Event</h1>
+            </div>
+            <nav className="main-navigation__items">
+                <ul>
+                    {!context.token && (<li>
+                        <NavLink to="/auth">Authentication</NavLink>
+                    </li>)}
+                    <li>
+                        <NavLink to="/events">Events</NavLink>
+                        </li>
+                    {context.token && (<li>
+                        <NavLink to="/Bookings">Bookings</NavLink>
+                    </li>)}
+                </ul>
+            </nav>
+        </header>)
+        }}
+    </AuthContext.Consumer>
 );
 
 export default mainNavigation;
