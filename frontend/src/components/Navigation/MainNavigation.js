3244b4b7bad0ed60f1ece287be5feb7e23834306
gabhijeetxsingh <abhijeetvns03@gmail.com>
2020-01-19 01:03:01 +0530

    added modal

--- frontend/src/components/Navigation/MainNavigation.js
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -18,9 +18,16 @@ const mainNavigation = props => (
                     <li>
                         <NavLink to="/events">Events</NavLink>
                         </li>
-                    {context.token && (<li>
-                        <NavLink to="/Bookings">Bookings</NavLink>
-                    </li>)}
+                    {context.token && (
+                    <React.Fragment>
+                        <li>
+                            <NavLink to="/Bookings">Bookings</NavLink>
+                        </li>
+                        <li>
+                            <button onClick={context.logout}>Logout</button>
+                        </li>
+                    </React.Fragment>
+                    )}
                 </ul>
             </nav>
         </header>)
