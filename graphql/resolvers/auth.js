222a426a06f2ef0b59c0cf9ed93a0e1d61aced2f
gabhijeetxsingh <abhijeetvns03@gmail.com>
2020-01-18 15:27:48 +0530

    fixes

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -2,16 +2,6 @@ const bcrypt= require("bcrypt");
 const jwt= require("jsonwebtoken");
 const User= require("../../models/user");
 
-const transformBooking = booking => {
-    return  {...booking._doc, 
-        _id: booking.id,
-        user : user.bind(this, booking._doc.user),
-        event: singleEvent.bind(this, booking._doc.event),
-        createdAt : dateToString(booking._doc.createdAt),
-        updatedAt : dateToString(booking._doc.updatedAt)
-    }
-}
-
 module.exports = {
     createUser:async (args) => {
         try {
